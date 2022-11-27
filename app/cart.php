b9783f6c084c8a57a59ca9b5d0649c0cb8d1aa10
rangguy <ranggadwi100@gmail.com>
2022-11-27 21:28:13 +0700

    perubahan di modal

--- app/cart.php
+++ app/cart.php
@@ -19,6 +19,7 @@ if ($delPesanan != "") {
 ?>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 
 <body>
 	<div id="pesanan">
@@ -57,56 +58,61 @@ if ($delPesanan != "") {
 					<td> $harga</td>
 				";
 			?>
+			<!-- <script>
+				$('#submitBtn').click(function() {
+					$('#tunai').text($('#uangBayar').val());
+				});
+			</script> -->
 			<td>
-				Tunai
-				<input type="text" name="uangBayar" id="uangBayar" size="4"><br>
-				<!-- Button trigger modal-->
-				<button type="button" class="btn btn-primary mt-2" data-toggle="modal" data-target="#exampleModal">
-					Bayar
-				</button>
+				<form action="<?php $_SERVER['PHP_SELF']; ?>" role="form" name="myform" method="POST" id="form">
+					Tunai <input type="text" name="uangBayar" id="uangBayar" size="4"><br>
+					<!-- Button trigger modal-->
+					<input type="button" name="btn" id="submitBtn" value="Bayar" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary mt-2">
+				</form>
 
 				<!-- Modal -->
 				<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 					<div class="modal-dialog modal-dialog-scrollable" role="document">
 						<div class="modal-content">
 							<div class="modal-header">
-								<h5 class="modal-title" id="exampleModalLabel">Bayar</h5>
+								<h5 class="modal-title" id="exampleModalLabel">Warung Siti</h5>
 								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 									<span aria-hidden="true">&times;</span>
 								</button>
 							</div>
 							<div class="modal-body">
-								<table width="100%">
+								<table width="100%" id="modal_body">
 									<tr>
 										<th>Item</th>
 										<th>Harga</th>
 									</tr>
 									<?php
 									$i = 0;
+									$tunai = intval($_POST['uangBayar']);
+									$kembalian = $tunai - $harga;
 									$sql = "SELECT * from pesanan";
 									$query = mysqli_query($conn, $sql);
 									while ($row = mysqli_fetch_array($query)) {
+										$i++;
 										echo "
 										<tr>
 											<td>$row[nama] x $row[jumlah]</td>
 											<td>$row[totalHarga]</td>
 										</tr>
 										";
-										$i++;
 									}
-									echo "<tr>
-									<td><b>Total Harga</b></td>
-									<td> $harga</td>
+									?>
+									<tr>
+										<th>Total Harga</th>
+										<th><?php echo $harga; ?></th>
 									</tr>
 									<tr>
-									<td><b>Tunai</b></td>
-									<td>$tunai</td>
+										<th>Tunai</th>
+										<th id="tunai"><?php echo $_POST['uangBayar'];?></th>
 									</tr>
-									<td><b>Kembalian</b></td>
-									<td>$kembalian</td>
+									<th>Kembalian</th>
+									<th><?php echo $kembalian; ?></th>
 									</tr>
-									";
-									?>
 								</table>
 							</div>
 							<div class="modal-footer">
